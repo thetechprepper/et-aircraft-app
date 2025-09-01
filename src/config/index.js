53de8aec0ace15c180f68f613af628fb2f114a64
thetechprepper <info@thetechprepper.com>
2025-09-01 08:36:20 -0700

    Changed default map tile server port from HTTP port 8000 to 1982 due to conflict with Dire Wolf's AGWPE port

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 // Hosts
 export const API_HOST  = import.meta.env.VITE_API_HOST  || 'http://localhost:1981';
 export const DATA_HOST = import.meta.env.VITE_DATA_HOST || 'http://localhost:1090';
-export const MAP_HOST  = import.meta.env.VITE_MAP_HOST  || 'http://localhost:8000';
+export const MAP_HOST  = import.meta.env.VITE_MAP_HOST  || 'http://localhost:1982';
 
 // Endpoints
 export const ADSB_SERVICE = `${DATA_HOST}/data.json`;
